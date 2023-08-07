1b727e5a45c7557d3c758576c58a3b60507cd918
Pedro B B Fonseca <84049639+PedroFonseca447@users.noreply.github.com>
2023-08-06 23:35:31 -0300

    componente task inserido

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+
+import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  //raiz da aplicação tudo que é passado dentro do.render
+  //é oq vai aparecer
+  //defende o codigo esse strictMode
   <React.StrictMode>
     <App />
   </React.StrictMode>,
